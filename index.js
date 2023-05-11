bc690c355f59892d7811458215ca244909fa37d1
msaadullah04@gmail.com <mac@macs-MacBook-Pro.local>
2023-05-12 03:26:11 +0500

    cors fix

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ connectDb()
 app.use(express.json())
 
 app.use(function(req, res, next) {
-    // res.header("Access-Control-Allow-Origin", "*");
-    const allowedOrigins = ['http://localhost:3000', 'http://car-rental-app-dashboard.netlify.app', 'https://car-rental-app-dashboard.netlify.app/'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-         res.setHeader('Access-Control-Allow-Origin', origin);
-    }
+    res.header("Access-Control-Allow-Origin", "*");
+    // const allowedOrigins = ['http://localhost:3000', 'http://car-rental-app-dashboard.netlify.app', 'https://car-rental-app-dashboard.netlify.app/'];
+    // const origin = req.headers.origin;
+    // if (allowedOrigins.includes(origin)) {
+    //      res.setHeader('Access-Control-Allow-Origin', origin);
+    // }
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-credentials", true);
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
