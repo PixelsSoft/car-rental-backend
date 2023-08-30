63f1cecec0d154174f0838a2c132f9c70ed0c5a6
taimoork-o16labs <taimoorkhan@o16-labs.net>
2023-08-30 05:08:59 +0500

    upload changes

--- src/routes/ItemRoutes.ts
+++ src/routes/ItemRoutes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+
 import {
   getItemById,
   getItems,
@@ -6,11 +7,23 @@ import {
   deleteItem,
   createItem,
 } from "../controllers/ItemController";
-import upload from "../helpers/upload";
+// import upload from "../helpers/upload";
+import multer from "multer";
 
+
+////// for test//////
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../../uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 const Router = express.Router();
 
-Router.post("/items/create", upload.array("images", 10), createItem);
+Router.post("/items/create", upload.single("image"), createItem);
 Router.put("/items/edit/:id", upload.array("images", 10), editItem);
 
 Router.get("/items", getItems);
